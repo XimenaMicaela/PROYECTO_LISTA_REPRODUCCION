42f56609fb4333b7df2299487d890246e16e7358
micapd <ximenapazmino7613@gmail.com>
2024-02-03 13:42:53 -0500

    actualización 03-02-2024

---
+++ main.js
@@ -0,0 +1,20 @@
+
+const input = document.getElementById('input')
+const boton = document.getElementById('buscar')
+const lista = document.getElementById('lista')
+
+const canciones = ['cancion de duki', 'cancion de emilia', 'cancion de tini ', 'otra de duki']
+
+boton.addEventListener('click', function () {
+  const valor = input.value
+
+
+  const cancionesQueCumplen = canciones.filter(c => c.includes(valor))
+
+  lista.innerHTML = cancionesQueCumplen.map(cancionQueCumplio => `<li>${cancionQueCumplio}</li>`)
+  if (cancionesQueCumplen.length === 0) {
+    lista.innerHTML = '<li>No se encontraron canciones</li>'
+  }
+})
+
+
