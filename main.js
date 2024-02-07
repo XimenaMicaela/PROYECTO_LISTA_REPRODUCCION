58db1d97ab8570f53f99e61fca193282baa2c826
micapd <ximenapazmino7613@gmail.com>
2024-02-06 21:22:39 -0500

    Java Reproductor

--- main.js
+++ main.js
@@ -18,3 +18,117 @@ boton.addEventListener('click', function () {
 })
 
 
+class Song {
+  constructor(nombre, artista, duracion, año, genero) {
+    this.nombre = nombre;
+    this.artista = artista;
+    this.duracion = duracion;
+    this.año = año;
+    this.genero = genero;
+  }
+
+
+}
+/* PARA REVISAR */
+
+/* getSongName () : string {
+  return `${this.nombre};
+}
+
+getSongArtist () : string {
+  return `${this.artista};
+}
+
+getSongDuration () : string {
+  return `${this.duration};
+}
+getSongYear () : string {
+  return `${this.año};
+}
+getSongGenre () : string {
+  return `${this.genero};
+} */
+
+
+class Playlist {
+
+  constructor(nombre, listaCanciones, ordenEscucha) {
+    this.nombre = nombre;
+    this.listaCanciones = listaCanciones;
+    this.ordenEscucha = ordenEscucha;
+  }
+}
+
+/* PARA REVISAR */
+/* getPlaylistName() {
+  return this.nombre;
+
+}
+
+getPlaylistSongs(){
+  return this.listaCanciones;
+}
+
+getPlaylistListeningOrder(){
+  return this.ordenEscucha;
+}
+
+
+
+addSongToPLaylist(song): void {
+  this.listaCanciones.push(song);
+
+}
+
+removeSongFromPlaylist(song): void {
+  this.listaCanciones = this.listaCanciones.filter(s > s !== song);
+}
+
+shufflePlaylist(): void {
+  this.listaCanciones = this.listaCanciones.sort(() => Math.random() - 0.5);
+}
+
+playPlaylist() : void {
+  this.listaCanciones.forEach(song => {
+    console.log(`Playing : ${song.nombre}`);
+
+  });
+} */
+
+class Reproductor {
+  catalogodeCanciones;
+  currentSong;
+  currentPlaylist;
+
+
+/* COMPLETAR CATALOGO CANCIONES */
+  constructor() {
+    this.catalogodeCanciones = [
+      new Song(nombre:"...", artista : "...", duracion: "", año : "", genero: "")
+
+    ]
+    this.mostrarCanciones();
+    this.currentSong = this.catalogodeCanciones[0];
+  }
+}
+
+
+/*  METODOS */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
